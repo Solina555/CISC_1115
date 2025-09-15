439d0155825799f935675f6b367288f05575ec35
Solina <solinalin1@gmail.com>
2025-09-15 13:28:23 -0400

    9/15/25 Lab

---
+++ Week_4/Lab_1.java
@@ -0,0 +1,18 @@
+package Week_4;
+
+import java.util.Scanner;
+
+public class Lab_1 {
+    // Lab: Write a program that reads in 3 integers and prints the smallest number of the 3.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        int smallestNumber = Math.min(num1, Math.min(num2, num3));
+        System.out.println("Smallest Number: " + smallestNumber);
+    }
+}
